db0384d11d221cf4a4fbbf843ceba97684248789
80bble <gitta98@ymail.com>
2025-10-20 13:29:37 +0200

    Tried adding Module creation mode -- Issues with https middleware connection. Tried editing classes for foreign keys Feilemelding: Microsoft.AspNetCore.HttpsPolicy.HttpsRedirectionMiddleware[3]       Failed to determine the https port for redirect.

---
+++ Albatross/ViewModels/ModuleTopViewModel.cs
@@ -0,0 +1,18 @@
+using Albatross.Models;
+
+namespace Albatross.ViewModels
+{
+    public class ModuleTopViewModel
+    {
+
+        public IEnumerable<ModuleTopic> ModuleTopics;  //{get; set;}
+
+        public string? CurrentViewName; //{get; set;}
+
+        public ModuleTopViewModel(IEnumerable<ModuleTopic> moduleTopics, string? currentViewName)
+        {
+            ModuleTopics = moduleTopics;
+            CurrentViewName = currentViewName;
+        }
+    }
+}

c4e7640e6af4c5c8cad1ef7ba064b3dce77a1e0f
80bble <gitta98@ymail.com>
2025-11-03 15:13:09 +0100

    Admin create moduletopic button works. Added buttons inside the accordion

--- Albatross/Migrations/20251103135244_MakeModuleIdNullable2.Designer.cs
+++ Albatross/Migrations/20251103135244_MakeModuleIdNullable2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Albatross.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Albatross.Migrations
 {
     [DbContext(typeof(ItemDbContext))]
-    partial class ItemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251103135244_MakeModuleIdNullable2")]
+    partial class MakeModuleIdNullable2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.21");
